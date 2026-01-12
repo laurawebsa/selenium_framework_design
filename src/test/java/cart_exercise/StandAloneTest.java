48e90057357fa5617a2524016402ae4be6867914
“Laura <lauraguep@gmail.com>
2026-01-12 17:29:13 -0500

    Initial commit selenium framework design project

---
+++ src/test/java/cart_exercise/StandAloneTest.java
@@ -0,0 +1,17 @@
+package cart_exercise;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StandAloneTest {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://rahulshettyacademy.com/client/");
+
+    }
+}
