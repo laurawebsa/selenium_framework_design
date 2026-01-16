321d29a0ba0927227147edba77b91ab7f08a6cb7
“Laura <lauraguep@gmail.com>
2026-01-15 19:47:46 -0500

    update project

--- src/test/java/cart_exercise/StandAloneTest.java
+++ src/test/java/cart_exercise/StandAloneTest.java
@@ -1,8 +1,18 @@
 package cart_exercise;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class StandAloneTest {
 
@@ -10,8 +20,38 @@ public class StandAloneTest {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-
+        //Global timeout
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://rahulshettyacademy.com/client/");
+        driver.findElement(By.id("userEmail")).sendKeys("laurawebsa@gmail.com");
+        driver.findElement(By.id("userPassword")).sendKeys("L@uris1608");
+        driver.findElement(By.id("login")).click();
+
+        WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+        List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+        //java streams
+        //The stream help us to iterate through all products
+        WebElement productSearch = products.stream().filter(product ->
+                product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
+                //click on Add to cart button
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement addToCartBtn = productSearch.
+                findElement(By.cssSelector(".card-body button:last-of-type"));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView({block:'center'});", addToCartBtn);
+
+        wait.until(ExpectedConditions.elementToBeClickable(addToCartBtn));
+        js.executeScript("arguments[0].click();", addToCartBtn);
+
+        //click on cart button to check the selections
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+        //ng-animating animated class
+        wait1.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+        driver.findElement(By.cssSelector("[routerlink *='cart']")).click();
+
+
 
     }
 }
